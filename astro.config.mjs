449a631c65bf04e19babae2808c7c84a91873d87
pokja3 <pokja3jkt@gmail.com>
2026-04-28 15:21:06 +0700

    fix vercel astro config 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
-import 'dotenv/config';
 
 export default defineConfig({
   integrations: [react()],
